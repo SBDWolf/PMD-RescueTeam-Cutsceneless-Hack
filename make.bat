68a58c045538516b372e8a9525dd6a188a021443
SBDWolf <90091268+SBDWolf@users.noreply.github.com>
2023-11-24 06:11:24 +0100

    build env fixes + intro done

--- make.bat
+++ make.bat
@@ -4,4 +4,4 @@ set rom_name=PMD_BRT_Cutsceneless_v%rom_version%.nds
 IF NOT EXIST rom_output mkdir rom_output
 del /q rom_output
 copy rom_source\source.nds rom_output\%rom_name%
-python .\script_import.py .\rom_source\source.nds
+python .\script_import.py .\rom_output\%rom_name%
